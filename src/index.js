6862f837a49c1a1128201043a492e1f918803d75
Aviscad <anthonyviscad@gmail.com>
2022-09-15 21:02:19 -0600

    Home page and webpack config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hi!");
+import "./css/style.css";
+import homeContent from "./home";
+
+homeContent();
+
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const contact = document.querySelector("#contact");
+
+home.onclick = () => {
+  console.log("Home");
+};
+menu.onclick = () => {
+  console.log("Menu");
+};
+contact.onclick = () => {
+  console.log("Contact");
+};
